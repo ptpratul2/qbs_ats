44fbaa6c31f805e9dbee60d25560a11d5810690d
ptpratul2 <ptpratul2@gmail.com>
2025-08-22 17:12:41 +0530

    New Commit

---
+++ qbs_ats/qbs_ats/doctype/client_contact/client_contact.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Prompt Personnel and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Client-Contact", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ qbs_ats/qbs_ats/doctype/new_client/test_new_client.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Prompt Personnel and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestNewClient(FrappeTestCase):
+	pass
