f3ab4a21f3fcf4911fcf995bb23c90a9b13b714e
Shalindra-coder <shalindrapaoriya23@navgurukul.org>
2025-11-13 10:38:07 +0530

    add placement doctype doctype details

---
+++ qbs_ats/qbs_ats/doctype/placement_details/placement_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Prompt Personnel and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Placement Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
