31c65a69bf730310d94be8a2fad47178eff79cf4
Shalindra-coder <shalindrapaoriya23@navgurukul.org>
2025-11-14 09:56:08 +0530

    add child table

---
+++ qbs_ats/qbs_ats/doctype/vendor_contact_details/vendor_contact_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Prompt Personnel and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Vendor Contact Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
