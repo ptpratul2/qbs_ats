d3f2357a1a3b13ffdd0a5d6196e11175cd8c8671
Shalindra-coder <shalindrapaoriya23@navgurukul.org>
2026-04-09 12:31:07 +0530

    fixed set applicant name and client name in Interview

--- qbs_ats/__init__.py
+++ qbs_ats/__init__.py
@@ -1,1 +1,9 @@
 __version__ = "0.0.1"
+
+
+
+import hrms.hr.doctype.job_applicant.job_applicant as job_applicant
+
+from qbs_ats.qbs_ats.set_applicant_name import custom_create_interview
+
+job_applicant.create_interview = custom_create_interview
